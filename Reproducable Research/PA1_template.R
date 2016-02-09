b45d8c609509664028c66c2e93eadffcf1f89b44
Dane Allen <danethepain83@gmail.com>
2016-02-09 14:00:18 -0700

    Update PA1_template.R

--- Reproducable Research/PA1_template.R
+++ Reproducable Research/PA1_template.R
@@ -14,9 +14,7 @@ usePackage <- function(p)
 
 usePackage("ggplot2")
 
-
 ## ------------------------------------------------------------------------
-library(ggplot2)
 total.steps <- tapply(data$steps, data$date, FUN=sum, na.rm=TRUE)
 qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
 mean(total.steps, na.rm=TRUE)
@@ -24,7 +22,6 @@ median(total.steps, na.rm=TRUE)
 
 
 ## ------------------------------------------------------------------------
-library(ggplot2)
 averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
                       FUN=mean, na.rm=TRUE)
 ggplot(data=averages, aes(x=interval, y=steps)) +
